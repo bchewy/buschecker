9b24258a489fa185b057d9fe6c1518bcd31154a4
Brian Chew <brian@bchewy.com>
2025-11-28 21:54:48 +0800

    Initial Commit

---
+++ buschecker/buscheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  buscheckerApp.swift
+//  buschecker
+//
+//  Created by brianchew on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct buscheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
